159623fda8cb2fcf7d3bdbca81e9d25f3e33f37b
Shamil Omarov <s_omarov05@mail.ru>
2020-05-18 18:15:26 +0300

    поправил ошибки в примерах

--- examples.js
+++ examples.js
@@ -13,13 +13,16 @@ example1();
     task - 2) Выведи с помощью цикла фразу по частям "Ура! Ура! Ура! Это победа!"
 */
 function example2() {
-    let arr = ['Ура!', 'Ура!', 'Ура!', 'Это победа!'];
+    const arr = ['Ура!', 'Ура!', 'Ура!', 'Это победа!'];
 
-    let i = 0;
-    while (i < arr.length) {
-        console.log(arr[i]);
-        i++;
+    for (let i = 0; i < arr.length; i++) {     // 1 способ )
+      console.log(arr[i]);
     }
+
+
+    // arr.forEach((item) => {     // 2 способ )
+    //     console.log(item);
+    // });
 }
 example2();
 
@@ -28,14 +31,20 @@ example2();
     task - 3) Выведи в консоль все нечетные числа от 10 до 0, по убыванию
 */
 function example3() {
-    let num = 10;
+    // let num = 10;
+    // while(num > 0) {          // 1 способ )
+    //     num--;
+    //     if (num % 2 === 0) {
+    //   	    continue;
+    //     }
+    //     console.log(num);
+    // }
 
-    while(num > 0) {
-        num--;
-        if (num % 2 === 0) {
-      	    continue;
+
+    for (let i = 10; i > 0; i--) {     // 2 способ )
+        if ( i % 2 !== 0 ) {
+          console.log(i);
         }
-        console.log(num);
     }
 }
 example3();
